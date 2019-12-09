84ac2b9e6d75917fd0cb2f50edc9c1db11f5c30b
Tugay Cem Evci <46964249+tugayevci@users.noreply.github.com>
2019-12-09 22:17:33 +0300

    initialCommit

---
+++ DataAccess/Concrete/EntityFramework/EfUserDal.cs
@@ -0,0 +1,28 @@
+﻿using Core.DataAccess.EntityFramework;
+using Core.Entities.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfUserDal : EfEntityRepositoryBase<User, NorthwindContext>, IUserDal
+    {
+        public List<OperationClaim> GetClaims(User user)
+        {
+            using (var context = new NorthwindContext())
+            {
+                var result = from o in context.OperationClaims
+                             join u in context.UserOperationClaims
+                             on o.Id equals u.OperationClaimId
+                             where u.UserId == user.Id
+                             select new OperationClaim { Id = o.Id, Name = o.Name };
+                return result.ToList();
+            }
+
+        }
+    }
+}
